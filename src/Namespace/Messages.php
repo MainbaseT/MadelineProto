25faeb1334dc74ad04f7350f949ac556225ff58b
Daniil Gentili <daniil@daniil.it>
2026-05-11 13:36:20 +0200

    Fix type

--- src/Namespace/Messages.php
+++ src/Namespace/Messages.php
@@ -3233,8 +3233,9 @@ interface Messages
      * @param ?int $floodWaitLimit Can be used to specify a custom flood wait limit: if a FLOOD_WAIT_ rate limiting error is received with a waiting period bigger than this integer, an RPCErrorException will be thrown; otherwise, MadelineProto will simply wait for the specified amount of time. Defaults to the value specified in the settings: https://docs.madelineproto.xyz/PHP/danog/MadelineProto/Settings/RPC.html#setfloodtimeout-int-floodtimeout-self
      * @param ?string $queueId If specified, ensures strict server-side execution order of concurrent calls with the same queue ID.
      * @param ?\Amp\Cancellation $cancellation Cancellation
+     * @return array{_: 'inputBotInlineMessageID', dc_id: int, id: int, access_hash: int}|array{_: 'inputBotInlineMessageID64', dc_id: int, owner_id: int, id: int, access_hash: int} @see https://docs.madelineproto.xyz/API_docs/types/InputBotInlineMessageID.html
      */
-    public function setBotGuestChatResult(array $result, int|null $query_id = 0, ?int $floodWaitLimit = null, ?string $queueId = null, ?\Amp\Cancellation $cancellation = null): bool;
+    public function setBotGuestChatResult(array $result, int|null $query_id = 0, ?int $floodWaitLimit = null, ?string $queueId = null, ?\Amp\Cancellation $cancellation = null): array;
 
     /**
      *
